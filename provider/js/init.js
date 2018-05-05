eea4229816d689a56bee6dcf9f75bde97c4a3be2
Daniel Cataño Restrepo <dasniel19910@gmail.com>
2018-05-05 16:18:21 -0500

    Add product

--- provider/js/init.js
+++ provider/js/init.js
@@ -3,3 +3,8 @@ M.Sidenav.init(sideNav, {})
 
 const collapsible = document.querySelectorAll('.collapsible');
 M.Collapsible.init(collapsible, {});
+
+document.querySelector("#productName").addEventListener("click", () => {
+    const item = document.querySelector("#quantityFieldWrapper");
+    item.classList.contains("add-field-collapsible") ? item.classList.remove("add-field-collapsible") : item.classList.add("add-field-collapsible");
+})
