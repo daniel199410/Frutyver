00102a2a1b23cf78512c381f59506e9a6fe8c409
Daniel Cataño Restrepo <dasniel19910@gmail.com>
2018-05-05 15:20:43 -0500

    Aside

--- provider/js/init.js
+++ provider/js/init.js
@@ -1,2 +1,5 @@
 const sideNav = document.querySelector('.sidenav')
 M.Sidenav.init(sideNav, {})
+
+const collapsible = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsible, {});
