b02efbe829f18d9621c6586fb081578099819df5
jxebastian <josrodriguezca@unal.edu.co>
2018-05-10 13:34:30 -0500

    Comezando historial de pedidos

---
+++ js/Collapsible.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instance = M.Collapsible.init(elems);
+});
--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.collapsible');
-  var instance = M.Collapsible.init(elems);
-  
-  var elemss = document.querySelectorAll('.modal');
-  var instances = M.Modal.init(elemss);
-
-var elems1= document.querySelectorAll('.sidenav'); var instances = M.Sidenav.init(elems1);
-});
